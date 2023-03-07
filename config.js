a8361ec31ded7cac374967e1e1d6140919f7c5a4
vyasshivansh25 <118053495+vyasshivansh25@users.noreply.github.com>
2023-03-07 04:47:18 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6h5k8RCrzvjrH7T99rYW6myA8eVGRSAg",
+  authDomain: "the-jungle-race-578fa.firebaseapp.com",
+  projectId: "the-jungle-race-578fa",
+  storageBucket: "the-jungle-race-578fa.appspot.com",
+  messagingSenderId: "201596531649",
+  appId: "1:201596531649:web:1c8761852c0149ba6df540"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
